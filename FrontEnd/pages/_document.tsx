e36d1250aee11e3dcb86c8227cd3ae87cb89811b
shuhaibw <120868621+shuhaibw@users.noreply.github.com>
2024-08-03 13:33:48 -0700

    Update document.tsx to add favicon

--- FrontEnd/pages/_document.tsx
+++ FrontEnd/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+         <meta charSet="UTF-8" />
+         <link rel="icon" href="astronaut.png" type="image/x-icon" />
+      </Head>
       <body>
         <Main />
         <NextScript />
